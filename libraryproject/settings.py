9fb321ff83bd0648c41e021a80dd5fe4cfd11746
Aleen <aleen.al-qwaifel@outlook.com>
2026-02-16 18:17:56 +0300

    update project

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -9,11 +9,16 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
+TEMPLATES = [{
+
+'DIRS': [TEMPLATE_DIR],
+
+}]
+
 
 
 # Quick-start development settings - unsuitable for production
--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+import apps.bookmodule.views
+from django.urls import include, path
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', apps.bookmodule.views.index),  #add only this line of code
+#     path('index2/<int:val1>/', apps.bookmodule.views.index2)  #add only this line
+# ]
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', include("apps.bookmodule.urls")), #include urls.py of bookmodule app
+    path('users/', include("apps.usermodule.urls"))  #include urls.py of usermodule app
 ]
